efdf6014047a5495f5f6d37dd74787ae1969a9dc
Kurtis Kemple <kurtiskemple@gmail.com>
2019-05-02 11:15:40 -0400

    add: missing commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { API } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react";
 
 function App() {
+  const [state, setState] = useState("");
+  useEffect(() => {
+    const callLambda = async () => {
+      const result = await API.put("nycjsamplify", "/helloworld");
+      setState(result.success);
+    };
+
+    callLambda();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{`Result is: ${state}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
